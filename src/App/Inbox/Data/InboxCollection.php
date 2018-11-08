2412d16a004c962a195459293ba22fa7fb8a84c0
nemundo <urs.lang@gmail.com>
2018-11-08 08:50:21 +0100

    .

--- src/App/Inbox/Data/InboxCollection.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace Nemundo\Workflow\App\Inbox\Data;
-use Nemundo\Model\Collection\AbstractModelCollection;
-class InboxCollection extends AbstractModelCollection {
-protected function loadCollection() {
-$this->addModel(new \Nemundo\Workflow\App\Inbox\Data\Inbox\InboxModel());
-}
-}
