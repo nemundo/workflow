d95d8b23f03c43cb28418fcec3f9524339429328
nemundo <urs.lang@gmail.com>
2019-01-14 16:18:31 +0100

    .

--- src/App/Workflow/Content/Type/AbstractStatusChangeWorkflowStaus.php
+++ src/App/Workflow/Content/Type/AbstractStatusChangeWorkflowStaus.php
@@ -4,9 +4,15 @@ namespace Nemundo\Workflow\App\Workflow\Content\Type;
 
 
 use Nemundo\Workflow\App\Workflow\Content\Form\StatusChangeForm;
+use Schleuniger\App\Task\Content\Type\Process\TaskProcess;
 
 abstract class AbstractStatusChangeWorkflowStaus extends AbstractWorkflowStatus
 {
+
+    /**
+     * @var TaskProcess
+     */
+    public $parentContentType;
 
     public function __construct($dataId = null)
     {
