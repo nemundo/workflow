0042787064401544a7fff4c668dd60146faf15ee
nemundo <urs.lang@gmail.com>
2019-03-21 10:18:46 +0100

    .

---
+++ src/Site/AbstractExcelSite.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Nemundo\Workflow\Site;
+
+
+use Nemundo\Package\FontAwesome\Site\AbstractIconSite;
+use Nemundo\Web\Site\AbstractSiteTree;
+
+abstract class AbstractExcelSite extends AbstractIconSite
+{
+
+    public function __construct(AbstractSiteTree $site = null)
+    {
+
+        $this->title = 'Excel Export';
+        $this->url = 'excel-export';
+
+        parent::__construct($site);
+
+        $this->icon->icon = 'file-excel';
+        $this->icon->iconSize = 3;
+
+    }
+
+}
