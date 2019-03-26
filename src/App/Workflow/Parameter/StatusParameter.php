1ce6d121bcdedb583ec79cf1d36d4db09baa9acf
nemundo <urs.lang@gmail.com>
2019-03-26 13:29:27 +0100

    .

--- src/App/Workflow/Parameter/StatusParameter.php
+++ src/App/Workflow/Parameter/StatusParameter.php
@@ -3,11 +3,13 @@
 namespace Nemundo\Workflow\App\Workflow\Parameter;
 
 use Nemundo\Web\Http\Parameter\AbstractUrlParameter;
+use Nemundo\Workflow\App\Workflow\Com\ListBox\Item\OpenListBoxItem;
 
 class StatusParameter extends AbstractUrlParameter
 {
     protected function loadParameter()
     {
         $this->parameterName = 'status';
+        $this->defaultValue = (new OpenListBoxItem())->id;
     }
 }
