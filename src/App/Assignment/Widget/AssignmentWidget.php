987417e75a508d57447623c74d907f308a850c61
nemundo <urs.lang@gmail.com>
2019-06-03 18:06:22 +0200

    .

--- src/App/Assignment/Widget/AssignmentWidget.php
+++ src/App/Assignment/Widget/AssignmentWidget.php
@@ -14,17 +14,16 @@ class AssignmentWidget extends AbstractAdminWidget
 
     protected function loadWidget()
     {
-<<<<<<< HEAD
-        $this->widgetTitle = 'Aufgaben';
-=======
+
+        //$this->widgetTitle = 'Aufgaben';
 
         $this->widgetTitle[LanguageCode::EN] = 'Assignment';
         $this->widgetTitle[LanguageCode::DE] = 'Zuweisungen';  // 'Aufgaben';
 
         //$this->widgetTitle = 'Aufgaben';  // (Assignment)';
 
->>>>>>> 16bc11b3f077d7ba5977bc26974b77bf5c19a229
         $this->widgetSite = AssignmentSite::$site;
+
     }
 
 
