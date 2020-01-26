a62f6238fc38b1ef332b04b1ef686aaa35dffba8
nemundo <urs.lang@gmail.com>
2020-01-26 22:45:58 +0100

    .

--- src/App/SearchEngine/Com/Navbar/NavbarSearchForm.php
+++ src/App/SearchEngine/Com/Navbar/NavbarSearchForm.php
@@ -3,10 +3,15 @@
 namespace Nemundo\Workflow\App\SearchEngine\Com\Navbar;
 
 
+use Nemundo\App\Search\Parameter\SearchQueryParameter;
 use Nemundo\Com\FormBuilder\SearchForm;
+use Nemundo\Html\Character\HtmlCharacter;
 use Nemundo\Html\Container\AbstractHtmlContainer;
 use Nemundo\Html\Form\Button\SubmitButton;
 use Nemundo\Core\Language\LanguageCode;
+use Nemundo\Package\Bootstrap\Autocomplete\BootstrapAutocompleteMultipleValueTextBox;
+use Nemundo\Process\Search\Site\SearchJsonSite;
+use Nemundo\Process\Search\Site\SearchSite;
 use Nemundo\Web\Site\AbstractSite;
 use Nemundo\Workflow\App\SearchEngine\Com\TextBox\SearchAutocompleteTextBox;
 use Nemundo\Workflow\App\SearchEngine\Parameter\QueryParameter;
@@ -24,13 +29,22 @@ class NavbarSearchForm extends AbstractHtmlContainer
     {
 
         $form = new SearchForm($this);  // new SiteForm($this);
-        $form->site = SearchEngineSite::$site;
+        $form->site = SearchSite::$site;  // SearchEngineSite::$site;
 
         $form->addCssClass('form-inline');
 
-        $input = new SearchAutocompleteTextBox($form);
+        $query = new BootstrapAutocompleteMultipleValueTextBox($form);
+        $query->name = (new SearchQueryParameter())->parameterName;
+        $query->seperator = ' ';
+        $query->searchMode = true;
+        $query->placeholder = 'Search';
+        $query->label = HtmlCharacter::NON_BREAKING_SPACE;
+        $query->sourceSite = SearchJsonSite::$site;
+        
+        
+        /*$input = new SearchAutocompleteTextBox($form);
         $input->name = (new QueryParameter())->getParameterName();
-        $input->addCssClass('mr-sm-2');
+        $input->addCssClass('mr-sm-2');*/
 
         $submit = new SubmitButton($form);
         $submit->label = 'Suchen';
